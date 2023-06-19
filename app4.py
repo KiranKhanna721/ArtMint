c00ad479b6da74b7154961cdf8f22bcf7549b473
Kiran Khanna <76143695+KiranKhanna721@users.noreply.github.com>
2023-06-19 08:18:42 +0530

    Update app4.py

--- app4.py
+++ app4.py
@@ -7,7 +7,7 @@ def generate_nft_artwork(description):
     response = openai.Image.create(
         prompt=prompt,
         n=1,
-        size="1024x1024"
+        size="256X256"
     )
     image_url = response['data'][0]['url']
     return image_url
